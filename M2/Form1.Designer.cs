a061cc37f1b84086bd96ff0715ef3a8a5287a2bc
henrique.carvalho <henrique.carvalho@HENRIQUE-CARVAL>
2022-11-25 15:31:47 -0300

    Add project files.

---
+++ M2/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace M2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_InscricaoMunicipal = new System.Windows.Forms.Label();
+            this.lb_InscEstadual = new System.Windows.Forms.Label();
+            this.lb_Cno = new System.Windows.Forms.Label();
+            this.lb_Caepf = new System.Windows.Forms.Label();
+            this.lb_CEI = new System.Windows.Forms.Label();
+            this.lb_Cpf = new System.Windows.Forms.Label();
+            this.lb_Cnpj = new System.Windows.Forms.Label();
+            this.lb_ExcecaoParRel = new System.Windows.Forms.Label();
+            this.lb_UnidadeContratante = new System.Windows.Forms.Label();
+            this.lb_Ativo = new System.Windows.Forms.Label();
+            this.txb_RazaoSocial = new System.Windows.Forms.TextBox();
+            this.lb_RazaoSocial = new System.Windows.Forms.Label();
+            this.txb_Nome = new System.Windows.Forms.TextBox();
+            this.lb_Nome = new System.Windows.Forms.Label();
+            this.txb_CodRh = new System.Windows.Forms.TextBox();
+            this.lb_CodRh = new System.Windows.Forms.Label();
+            this.txb_Codigo = new System.Windows.Forms.TextBox();
+            this.lb_Codigo = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ckb_Ativo = new System.Windows.Forms.CheckBox();
+            this.ckb_UnidadeContratante = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lb_InscricaoMunicipal
+            // 
+            this.lb_InscricaoMunicipal.AutoSize = true;
+            this.lb_InscricaoMunicipal.Location = new System.Drawing.Point(13, 358);
+            this.lb_InscricaoMunicipal.Name = "lb_InscricaoMunicipal";
+            this.lb_InscricaoMunicipal.Size = new System.Drawing.Size(98, 13);
+            this.lb_InscricaoMunicipal.TabIndex = 35;
+            this.lb_InscricaoMunicipal.Text = "Inscrição Municipal";
+            // 
+            // lb_InscEstadual
+            // 
+            this.lb_InscEstadual.AutoSize = true;
+            this.lb_InscEstadual.Location = new System.Drawing.Point(13, 332);
+            this.lb_InscEstadual.Name = "lb_InscEstadual";
+            this.lb_InscEstadual.Size = new System.Drawing.Size(74, 13);
+            this.lb_InscEstadual.TabIndex = 34;
+            this.lb_InscEstadual.Text = "Insc. Estadual";
+            // 
+            // lb_Cno
+            // 
+            this.lb_Cno.AutoSize = true;
+            this.lb_Cno.Location = new System.Drawing.Point(17, 306);
+            this.lb_Cno.Name = "lb_Cno";
+            this.lb_Cno.Size = new System.Drawing.Size(30, 13);
+            this.lb_Cno.TabIndex = 33;
+            this.lb_Cno.Text = "CNO";
+            // 
+            // lb_Caepf
+            // 
+            this.lb_Caepf.AutoSize = true;
+            this.lb_Caepf.Location = new System.Drawing.Point(16, 280);
+            this.lb_Caepf.Name = "lb_Caepf";
+            this.lb_Caepf.Size = new System.Drawing.Size(41, 13);
+            this.lb_Caepf.TabIndex = 32;
+            this.lb_Caepf.Text = "CAEPF";
+            // 
+            // lb_CEI
+            // 
+            this.lb_CEI.AutoSize = true;
+            this.lb_CEI.Location = new System.Drawing.Point(16, 254);
+            this.lb_CEI.Name = "lb_CEI";
+            this.lb_CEI.Size = new System.Drawing.Size(24, 13);
+            this.lb_CEI.TabIndex = 31;
+            this.lb_CEI.Text = "CEI";
+            // 
+            // lb_Cpf
+            // 
+            this.lb_Cpf.AutoSize = true;
+            this.lb_Cpf.Location = new System.Drawing.Point(16, 228);
+            this.lb_Cpf.Name = "lb_Cpf";
+            this.lb_Cpf.Size = new System.Drawing.Size(27, 13);
+            this.lb_Cpf.TabIndex = 30;
+            this.lb_Cpf.Text = "CPF";
+            // 
+            // lb_Cnpj
+            // 
+            this.lb_Cnpj.AutoSize = true;
+            this.lb_Cnpj.Location = new System.Drawing.Point(13, 202);
+            this.lb_Cnpj.Name = "lb_Cnpj";
+            this.lb_Cnpj.Size = new System.Drawing.Size(34, 13);
+            this.lb_Cnpj.TabIndex = 29;
+            this.lb_Cnpj.Text = "CNPJ";
+            // 
+            // lb_ExcecaoParRel
+            // 
+            this.lb_ExcecaoParRel.AutoSize = true;
+            this.lb_ExcecaoParRel.Location = new System.Drawing.Point(12, 176);
+            this.lb_ExcecaoParRel.Name = "lb_ExcecaoParRel";
+            this.lb_ExcecaoParRel.Size = new System.Drawing.Size(155, 13);
+            this.lb_ExcecaoParRel.TabIndex = 28;
+            this.lb_ExcecaoParRel.Text = "Exceção Parâmetros Relatórios";
+            // 
+            // lb_UnidadeContratante
+            // 
+            this.lb_UnidadeContratante.AutoSize = true;
+            this.lb_UnidadeContratante.Location = new System.Drawing.Point(13, 150);
+            this.lb_UnidadeContratante.Name = "lb_UnidadeContratante";
+            this.lb_UnidadeContratante.Size = new System.Drawing.Size(104, 13);
+            this.lb_UnidadeContratante.TabIndex = 27;
+            this.lb_UnidadeContratante.Text = "Unidade contratante";
+            // 
+            // lb_Ativo
+            // 
+            this.lb_Ativo.AutoSize = true;
+            this.lb_Ativo.Location = new System.Drawing.Point(13, 124);
+            this.lb_Ativo.Name = "lb_Ativo";
+            this.lb_Ativo.Size = new System.Drawing.Size(31, 13);
+            this.lb_Ativo.TabIndex = 26;
+            this.lb_Ativo.Text = "Ativo";
+            // 
+            // txb_RazaoSocial
+            // 
+            this.txb_RazaoSocial.Location = new System.Drawing.Point(201, 95);
+            this.txb_RazaoSocial.Name = "txb_RazaoSocial";
+            this.txb_RazaoSocial.Size = new System.Drawing.Size(331, 20);
+            this.txb_RazaoSocial.TabIndex = 24;
+            // 
+            // lb_RazaoSocial
+            // 
+            this.lb_RazaoSocial.AutoSize = true;
+            this.lb_RazaoSocial.Location = new System.Drawing.Point(13, 98);
+            this.lb_RazaoSocial.Name = "lb_RazaoSocial";
+            this.lb_RazaoSocial.Size = new System.Drawing.Size(70, 13);
+            this.lb_RazaoSocial.TabIndex = 25;
+            this.lb_RazaoSocial.Text = "Razão Social";
+            // 
+            // txb_Nome
+            // 
+            this.txb_Nome.Location = new System.Drawing.Point(201, 69);
+            this.txb_Nome.Name = "txb_Nome";
+            this.txb_Nome.Size = new System.Drawing.Size(331, 20);
+            this.txb_Nome.TabIndex = 22;
+            // 
+            // lb_Nome
+            // 
+            this.lb_Nome.AutoSize = true;
+            this.lb_Nome.Location = new System.Drawing.Point(13, 72);
+            this.lb_Nome.Name = "lb_Nome";
+            this.lb_Nome.Size = new System.Drawing.Size(39, 13);
+            this.lb_Nome.TabIndex = 23;
+            this.lb_Nome.Text = "*Nome";
+            // 
+            // txb_CodRh
+            // 
+            this.txb_CodRh.Location = new System.Drawing.Point(201, 43);
+            this.txb_CodRh.Name = "txb_CodRh";
+            this.txb_CodRh.Size = new System.Drawing.Size(331, 20);
+            this.txb_CodRh.TabIndex = 21;
+            // 
+            // lb_CodRh
+            // 
+            this.lb_CodRh.AutoSize = true;
+            this.lb_CodRh.Location = new System.Drawing.Point(13, 46);
+            this.lb_CodRh.Name = "lb_CodRh";
+            this.lb_CodRh.Size = new System.Drawing.Size(74, 13);
+            this.lb_CodRh.TabIndex = 20;
+            this.lb_CodRh.Text = "Código do RH";
+            // 
+            // txb_Codigo
+            // 
+            this.txb_Codigo.Location = new System.Drawing.Point(201, 17);
+            this.txb_Codigo.Name = "txb_Codigo";
+            this.txb_Codigo.Size = new System.Drawing.Size(224, 20);
+            this.txb_Codigo.TabIndex = 19;
+            // 
+            // lb_Codigo
+            // 
+            this.lb_Codigo.AutoSize = true;
+            this.lb_Codigo.Location = new System.Drawing.Point(13, 20);
+            this.lb_Codigo.Name = "lb_Codigo";
+            this.lb_Codigo.Size = new System.Drawing.Size(44, 13);
+            this.lb_Codigo.TabIndex = 18;
+            this.lb_Codigo.Text = "*Código";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(201, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 251);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(201, 277);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(201, 303);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(148, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(201, 329);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(201, 355);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(148, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(201, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // ckb_Ativo
+            // 
+            this.ckb_Ativo.AutoSize = true;
+            this.ckb_Ativo.Location = new System.Drawing.Point(201, 122);
+            this.ckb_Ativo.Name = "ckb_Ativo";
+            this.ckb_Ativo.Size = new System.Drawing.Size(15, 14);
+            this.ckb_Ativo.TabIndex = 5;
+            this.ckb_Ativo.UseVisualStyleBackColor = true;
+            // 
+            // ckb_UnidadeContratante
+            // 
+            this.ckb_UnidadeContratante.AutoSize = true;
+            this.ckb_UnidadeContratante.Location = new System.Drawing.Point(201, 149);
+            this.ckb_UnidadeContratante.Name = "ckb_UnidadeContratante";
+            this.ckb_UnidadeContratante.Size = new System.Drawing.Size(15, 14);
+            this.ckb_UnidadeContratante.TabIndex = 6;
+            this.ckb_UnidadeContratante.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 391);
+            this.Controls.Add(this.ckb_UnidadeContratante);
+            this.Controls.Add(this.ckb_Ativo);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_InscricaoMunicipal);
+            this.Controls.Add(this.lb_InscEstadual);
+            this.Controls.Add(this.lb_Cno);
+            this.Controls.Add(this.lb_Caepf);
+            this.Controls.Add(this.lb_CEI);
+            this.Controls.Add(this.lb_Cpf);
+            this.Controls.Add(this.lb_Cnpj);
+            this.Controls.Add(this.lb_ExcecaoParRel);
+            this.Controls.Add(this.lb_UnidadeContratante);
+            this.Controls.Add(this.lb_Ativo);
+            this.Controls.Add(this.txb_RazaoSocial);
+            this.Controls.Add(this.lb_RazaoSocial);
+            this.Controls.Add(this.txb_Nome);
+            this.Controls.Add(this.lb_Nome);
+            this.Controls.Add(this.txb_CodRh);
+            this.Controls.Add(this.lb_CodRh);
+            this.Controls.Add(this.txb_Codigo);
+            this.Controls.Add(this.lb_Codigo);
+            this.Name = "Form1";
+            this.Text = "Cadastro Funcionário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_InscricaoMunicipal;
+        private System.Windows.Forms.Label lb_InscEstadual;
+        private System.Windows.Forms.Label lb_Cno;
+        private System.Windows.Forms.Label lb_Caepf;
+        private System.Windows.Forms.Label lb_CEI;
+        private System.Windows.Forms.Label lb_Cpf;
+        private System.Windows.Forms.Label lb_Cnpj;
+        private System.Windows.Forms.Label lb_ExcecaoParRel;
+        private System.Windows.Forms.Label lb_UnidadeContratante;
+        private System.Windows.Forms.Label lb_Ativo;
+        private System.Windows.Forms.TextBox txb_RazaoSocial;
+        private System.Windows.Forms.Label lb_RazaoSocial;
+        private System.Windows.Forms.TextBox txb_Nome;
+        private System.Windows.Forms.Label lb_Nome;
+        private System.Windows.Forms.TextBox txb_CodRh;
+        private System.Windows.Forms.Label lb_CodRh;
+        private System.Windows.Forms.TextBox txb_Codigo;
+        private System.Windows.Forms.Label lb_Codigo;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox ckb_Ativo;
+        private System.Windows.Forms.CheckBox ckb_UnidadeContratante;
+    }
+}
+
---
+++ M2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txb_Codigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
